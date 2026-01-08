24033214503de6ce0d6db2d84a29c4c52efa416b
Siddharth A <146508401+002SIDDHARTH@users.noreply.github.com>
2026-01-08 23:58:55 +0530

    Refactor Index_Select.py for clarity and formatting

--- Index_Select.py
+++ Index_Select.py
@@ -1,6 +1,8 @@
+# IBM Coding Round Quetion
 arr=[10,20,30,40,50]
 index=int(input("Enter Your Index Value :"))
 if 0 <= index < len(arr):
     print(arr[index]//2)
 else:
     print("Index Out of Range")
+
